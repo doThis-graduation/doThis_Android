5589407f167b8366ed54bd3338bf249ae9c7e0f5
Hyoung Jin <drj9802@gmail.com>
2022-07-29 00:32:37 +0900

    google 로그인 준비

--- app/src/main/java/com/example/healthcare_exercise/MainActivity.kt
+++ app/src/main/java/com/example/healthcare_exercise/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.healthcare_exercise.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +16,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
     }
 
     //입력된 id, pw 읽어와서 파이어베이스와 비교. 로그인으로 넘어감.
