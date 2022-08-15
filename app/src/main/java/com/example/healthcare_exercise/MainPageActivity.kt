32bef609b522b1bed09c42e76037211dd671e098
Hyoung Jin <drj9802@gmail.com>
2022-08-16 04:48:36 +0900

    로그아웃버튼

--- app/src/main/java/com/example/healthcare_exercise/MainPageActivity.kt
+++ app/src/main/java/com/example/healthcare_exercise/MainPageActivity.kt
@@ -5,16 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.healthcare_exercise.databinding.ActivityMainPageBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainPageActivity : AppCompatActivity() {
 
     private var mBinding: ActivityMainPageBinding? = null
     private val binding get() = mBinding!!
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
     }
 
     override fun onBackPressed() {
@@ -24,6 +29,13 @@ class MainPageActivity : AppCompatActivity() {
     fun exerciseClicked(view: View) {
         val intent = Intent(this, ExerciseActivity::class.java)
         startActivity(intent)
+    }
+
+    fun logout(view: View) {
+        auth.signOut()
+        var intent=Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        this.finish()
     }
 
 }
