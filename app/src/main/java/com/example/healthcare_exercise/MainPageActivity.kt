69649f876f0c8a914eadf9ee6facb9653c9ae5e6
Hyoung Jin <drj9802@gmail.com>
2022-08-19 05:05:50 +0900

    fragment 연결 완료

--- app/src/main/java/com/example/healthcare_exercise/MainPageActivity.kt
+++ app/src/main/java/com/example/healthcare_exercise/MainPageActivity.kt
@@ -2,8 +2,13 @@ package com.example.healthcare_exercise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.healthcare_exercise.databinding.ActivityMainPageBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import fragment.BalanceFragment
+import fragment.CommunityFragment
 import fragment.ExerciseFragment
+import fragment.MyPageFragment
 
 class MainPageActivity : AppCompatActivity() {
 
@@ -15,11 +20,33 @@ class MainPageActivity : AppCompatActivity() {
         mBinding = ActivityMainPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val transaction = supportFragmentManager.beginTransaction().add(R.id.mainFrame, exerciseFragment())
-//        transaction.commit()
+        //default fragment 설정
         val transaction = supportFragmentManager.beginTransaction()
         transaction.add(R.id.mainFrame, ExerciseFragment())
         transaction.commit()
+
+        var bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+        bottomNav.run { setOnNavigationItemReselectedListener {
+            when(it.itemId){
+                R.id.bn_exercise ->{
+                    Toast.makeText(context, "E clicked", Toast.LENGTH_SHORT).show()
+                    supportFragmentManager.beginTransaction().replace(R.id.mainFrame, ExerciseFragment()).commit()
+                }
+                R.id.bn_balance ->{
+                    Toast.makeText(context, "B clicked", Toast.LENGTH_SHORT).show()
+                    supportFragmentManager.beginTransaction().replace(R.id.mainFrame, BalanceFragment()).commit()
+                }
+                R.id.bn_community ->{
+                    Toast.makeText(context, "C clicked", Toast.LENGTH_SHORT).show()
+                    supportFragmentManager.beginTransaction().replace(R.id.mainFrame, CommunityFragment()).commit()
+                }
+                R.id.bn_mypage ->{
+                    Toast.makeText(context, "M clicked", Toast.LENGTH_SHORT).show()
+                    supportFragmentManager.beginTransaction().replace(R.id.mainFrame, MyPageFragment()).commit()
+                }
+            }
+            true
+        } }
     }
 
     override fun onBackPressed() {
