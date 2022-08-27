33308d200d3d23d6542ce2a6259fe55d5bf0a562
Hyoung Jin <drj9802@gmail.com>
2022-08-28 01:57:29 +0900

    ExerciseUploadFragment.kt RadioButton -> Spinner로 method 선택 구현 method 카테고리 별로 firebase upload 성공
    ============
    upload 성공 후 분석 fragment로 넘어가는것 구현 필요
    (분석 결과를 서버에서 받아오는 fragment)
    
    balance 동영상 촬영 -> 미리보기(다시촬영), 업로드 -> 분석 fragment로 넘어가기 구현 필요

--- app/src/main/java/fragment/ExerciseUploadFragment.kt
+++ app/src/main/java/fragment/ExerciseUploadFragment.kt
@@ -1,13 +1,14 @@
 package fragment
 
-import android.content.Intent
+import android.content.Context
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.example.healthcare_exercise.R
 import com.google.firebase.storage.FirebaseStorage
@@ -40,10 +41,23 @@ class ExerciseUploadFragment : Fragment() {
         //사용자 정보 text set
         infoSet()
 
+        //스피너 구현
+        var methods = resources.getStringArray(R.array.methods)
+        var adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, methods)
+        this.viewProfile!!.spinner.adapter = adapter
+
+        this.viewProfile!!.spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+                method = methods.get(position)
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+        }
+
         //storage에 동영상 upload
         this.viewProfile!!.btn_analyse.setOnClickListener(View.OnClickListener { funImageUpload(viewProfile!!) })
 
-        //realtime database에 info upload
+        //realtime database에 info upload?? 필요??
 
         return viewProfile
     }
@@ -65,7 +79,7 @@ class ExerciseUploadFragment : Fragment() {
         var storageRef = fbStorage?.reference?.child(email)?.child(method)?.child(imgFileName)
 
         storageRef?.putFile(uri!!)?.addOnSuccessListener{
-            Toast.makeText(context,"Video Uploaded", Toast.LENGTH_SHORT).show()
+            Toast.makeText(context,"Video Uploaded_"+name+"_"+method, Toast.LENGTH_LONG).show()
         }
     }
 }
