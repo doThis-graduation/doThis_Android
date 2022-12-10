342671010664fa972bd3949b788662c41dd01e13
Hyoung Jin <drj9802@gmail.com>
2022-12-10 23:28:30 +0900

    ExerciseAnalyseFragment.kt -> firebase 에서 uri 읽어오기
    ================
    
    // wait response
    방법1. response 가 도착했음을 인식했을 때, 비교 후 changeFragment. 그때까지는 loading
    방법2. response 가 도착할 만한 충분한 시간을 sleep 하고, changeFragment. 그때까지는 loading
    
    progress bar / loading img 수정 필요
    
    ================
    1. BalanceFragment.kt => camera 실행코드 추가 오류 수정 필요
    
    2. 앨범에서 뒤로가기 누르면 미리보기 화면과, 업로드 버튼 visibility => BUG
    
    3. Exercise 3가지 fragment 에서 운동 video size 및 배율 조절 필요

--- app/src/main/java/fragment/ExerciseAnalyseFragment.kt
+++ app/src/main/java/fragment/ExerciseAnalyseFragment.kt
@@ -2,16 +2,19 @@ package fragment
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.healthcare_exercise.R
 import com.example.healthcare_exercise.databinding.FragmentExerciseAnalyseBinding
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_exercise_analyse.*
 import kotlinx.android.synthetic.main.fragment_exercise_upload.*
 import kotlinx.android.synthetic.main.fragment_exercise_upload.tx_userName
 import kotlinx.android.synthetic.main.fragment_exercise_upload.view.*
+import com.google.firebase.storage.FirebaseStorage
 
 class ExerciseAnalyseFragment : Fragment() {
 
@@ -23,7 +26,6 @@ class ExerciseAnalyseFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         binding = FragmentExerciseAnalyseBinding.inflate(inflater, container, false)
 
         var userName = arguments?.getString("name").toString()
@@ -35,6 +37,16 @@ class ExerciseAnalyseFragment : Fragment() {
         binding.txMethod.text = method
         binding.videoView.setVideoURI(uri)
         binding.videoView.start()
+
+        var storage = FirebaseStorage.getInstance()
+        var storageRef = storage.getReference()
+        storageRef.child("temp/image/celebrity.mp4").downloadUrl.addOnSuccessListener {
+            Log.d("불러오기","video download success")
+            binding.vdd.setVideoURI(it)
+            Log.d("불러오기","video 배치 완료")
+            binding.vdd.start()
+        }
+
 
 
         return binding.root
